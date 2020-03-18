b8fd6dba4777ba274001f227417702991d1af58f
henry22 <henry811010@gmail.com>
2020-03-18 11:39:23 +0800

    Angular-s2

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -9,8 +9,18 @@ export class MessageService {
   }
 
   clear() {
-    if(confirm('您確定要刪除訊息嗎?')) {
+    if (confirm('您確定要刪除訊息嗎?')) {
       this.messages = [];
+    }
+  }
+
+  clearTopFourMessages() {
+    if (confirm('您確定要清除頁面底部(Messages)裡的前四筆訊息嗎?')) {
+      if(this.messages.length <= 4) {
+        this.messages = []
+      } else {
+        this.messages.splice(0, 4);
+      }
     }
   }
 }
